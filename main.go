27da13bf148c2537f44afbc503211469caf8e0a0
Mark Pavlov <m7mark@yandex.ru>
2024-07-21 18:46:56 +0300

    add signup page

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ func main() {
 	t = views.Must(views.ParseFs(templates.FS, "faq.gohtml", "tailwind.gohtml"))
 	r.Get("/faq", controllers.FAQ(t))
 
+	t = views.Must(views.ParseFs(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", controllers.FAQ(t))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
